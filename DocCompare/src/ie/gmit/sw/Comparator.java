42118b6c7a4c430c842740522e47d14d697d740e
Rebecca <rebecca.kane@outlook.com>
2018-01-08 15:03:47 +0000

    Added javadoc comments to Comparator/Impl

--- DocCompare/src/ie/gmit/sw/Comparator.java
+++ DocCompare/src/ie/gmit/sw/Comparator.java
@@ -2,9 +2,21 @@ package ie.gmit.sw;
 
 import java.util.List;
 
+/**
+* <h1>Comparator Interface</h1>
+* The Comparator interface defines methods to be used in its implementation.
+*
+* @author  Rebecca Kane
+* @version 1.0
+*/
+
 public interface Comparator {
-	
-	// Compare method
-	public float compare(List<Integer>a, List<Integer>b); // Pass two sets (a and b) to the compare method
+	/**
+	* This is the compare method which calculates the similarity of two documents.
+   	* @param a			the list of hash codes generated from shingling the first document
+   	* @param b			the list of hash codes generated from shingling the second document
+   	* @return float		the similarity between a and b as a percentage / 100	
+   	*/
+	public float compare(List<Integer>a, List<Integer>b); // Pass two lists (a and b) to the compare method
 
 }
--- DocCompare/src/ie/gmit/sw/ComparisonImpl.java
+++ DocCompare/src/ie/gmit/sw/ComparisonImpl.java
@@ -2,16 +2,28 @@ package ie.gmit.sw;
 
 import java.util.List;
 
+/**
+* <h1>Comparison Implementation</h1>
+* The Comparison implementation implements the Comparator interface and compares the similarity between two given lists. 
+*
+* @author  Rebecca Kane
+* @version 1.0
+*/
+
 // Comparison handles anything to do with calculating similarity between docs, implements Comparator interface
 // Going by spec, Set sounds like a good collection type to use for now (- size/contains/retainAll, and no duplicates) 
-// 
 
 public class ComparisonImpl implements Comparator {
 	
 	// Do actual comparing stuff here
 	// Calculate similarity between docs
-	// Need somewhere to store identical shingles - map / array?
-
+	
+	/**
+	* This is the compare method which calculates the similarity of two documents.&nsbp;It takes two lists of Integers as parameters, performs calculations to determine the similarity of the two lists, and returns the result of these calculations as a percentage out of 100 - <i>i.e. 3.9 = 3.9%, 42 = 42%, 100 = 100% etc.</i>
+   	* @param a			the list of hash codes generated from shingling the first document
+   	* @param b			the list of hash codes generated from shingling the second document
+   	* @return float		the similarity between a and b as a percentage / 100	
+   	*/
 	public float compare(List<Integer> a, List<Integer> b) {
 		int size;
 		int similarities = 0;
@@ -36,9 +48,5 @@ public class ComparisonImpl implements Comparator {
 		
 		return percentage;
 	}
-	
-	
-	
-	
 
 }
